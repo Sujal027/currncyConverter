070247abdd6dfcd3dd0be6891a2a75ffcb5f4213
Sujal027 <148904402+Sujal027@users.noreply.github.com>
2024-07-28 13:01:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,227 @@
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+  };
+  const BASE_URL =
+  "https://open.er-api.com/v6/latest/";
+  const dropdowns = document.querySelectorAll(".dropdowns");
+for(select of dropdowns)
+{
+    console.log(select);
+
+}
+const updateflag = (element) => {
+    let a = countryList[element.value];
+ let b = `https://flagsapi.com/${a}/flat/64.png`;
+ if(element.name == "from")
+ {
+    document.querySelector("#from").setAttribute("src" , b) ;
+ }
+ else if(element.name == "to"){
+    document.querySelector("#to").setAttribute("src"  ,b);
+ }
+}
+  for(select of dropdowns)
+  {
+    for(currCode in countryList)
+    {
+        let e = document.createElement("option");
+        e.innerHTML = countryList[currCode];
+        e.value = currCode;
+        select.append(e);
+        if(select.name === "from" && e.value === "USD")
+            {
+                e.selected = "selected";
+            }
+            else if(select.name === "to" && e.value === "INR" )
+                {
+                    e.selected = "selected";
+    
+            }
+            
+    }
+    console.log(select.selected);
+    select.addEventListener("change" , (evt) => {
+        updateflag(evt.target);
+    })
+  
+   
+
+  }
+  const exchange = async () => {
+let entAmt = 0 ;
+let inp = document.querySelector("#inPUT");
+if(inp.value == '' || inp.value < 0)
+{
+    inp.value = 1;
+    entAmt = 1;
+}
+else 
+{
+    entAmt = Number.parseInt(inp.value);
+}
+let from = document.querySelector("#sfrom").value;
+let to = document.querySelector("#sto").value;
+let p = await fetch(`https://open.er-api.com/v6/latest/${from}`);
+let t = await p.json();
+let rate  = t.rates[to];
+document.querySelector("#msg").innerHTML = `${entAmt} ${from} = ${entAmt*rate} ${to}`;
+  }
+document.querySelector(".button-37").addEventListener("click" , exchange);
